ddef3d23a142b52c25ea7ae017ce90aab085bc0a
inseong0112 <inseong0112@gmail.com>
2025-08-13 22:48:04 +0900

    Co-authored-by: jueunlee0128 <jueunlee0128@users.noreply.github.com>

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,102 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Image } from 'expo-image';
+import { Tabs } from "expo-router";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+    return (
+        <Tabs
+        screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: '#FFFFFF',
+            tabBarInactiveTintColor: '#454547',
+            tabBarStyle: {
+                backgroundColor: '#121212',
+                borderTopWidth: 1,
+                borderTopColor: '#E5E5EA',
+                height: 60,
+                paddingBottom: 10,
+                paddingTop: 10,
+            },
         }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+        >
+            <Tabs.Screen 
+            name="index"
+            options={{
+                tabBarLabel: "홈",
+                tabBarIcon: ({ focused }) => (
+                    <Image source={
+                        focused
+                        ? require("@/assets/main_page_tp.png")
+                        : require("@/assets/main_page_white.png")
+                    } style={{width: 24, height: 24}} 
+                    />
+                )  
+            }}
+            />
+            <Tabs.Screen 
+            name="order" 
+            options={{ 
+                tabBarLabel: "주문", 
+                tabBarIcon: ({ focused }) => (
+                    <Image source={
+                        focused
+                        ? require("@/assets/savings_tp.png")
+                        : require("@/assets/savings_white.png")    
+                    } style={{width: 24, height: 24}}
+                    />
+                )    
+            }} 
+            />
+            <Tabs.Screen 
+            name="search" 
+            options={{ 
+                tabBarLabel: "탐색", 
+                tabBarIcon: ({ focused }) => (
+                    <Image source={
+                        focused
+                        ? require("@/assets/explore_tp.png")
+                        : require("@/assets/explore_white.png")
+                    } 
+                    style={{width: 24, height: 24}}
+                    />
+                )    
+            }} 
+            />
+            <Tabs.Screen 
+            name="game" 
+            options={{ 
+                tabBarLabel: "게임",
+                tabBarIcon: ({ focused }) => (
+                    <Image source={
+                        focused
+                        ? require("@/assets/images/sports_esports_tp.png")
+                        : require("@/assets/images/sports_esports_white.png")
+                    } 
+                    style={{width: 24, height: 24}}/>
+                )    
+            }} 
+            />
+            <Tabs.Screen 
+            name="tier" 
+            options={{ 
+                tabBarLabel: "랭킹",
+                tabBarIcon: ({ focused }) => (
+                    <Image source={
+                        focused
+                        ? require("@/assets/images/award_star_tp.png")
+                        : require("@/assets/images/award_star_white.png")
+                    } style={{width: 24, height: 24}}/>
+                )    
+            }} 
+            />
+            <Tabs.Screen 
+            name="profile" 
+            options={{ 
+                tabBarLabel: "마이", 
+                tabBarIcon: ({ focused }) => (
+                    <Image source={require("@/assets/images/account_circle.png")} style={{width: 24, height: 24}}/>
+                )
+            }} 
+            />
+        </Tabs>
+    );
 }
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+export default function Profile() {
+    return (
+        <View
+        style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+        }}>
+            <Text>edit app/(tabs)/profile.tsx</Text>
+        </View>
+    )
+}
